2834838240250e2e5dec4c6058bb54455f1c5a6c
Zulfikar Muhamad <zul.m@icloud.com>
2022-10-19 13:33:25 +0800

    Modified to remove secrets

--- beginner/send instagram messages/instagram_messages.py
+++ beginner/send instagram messages/instagram_messages.py
@@ -1,5 +1,5 @@
 from instabot import Bot
 bot = Bot()
 
-bot.login(username = "epift", password = "Epiftfoot08")
-bot.send_message("Hello meknol!", ["_ainulnordin_"])
+bot.login(username = "Your Username", password = "Your Password")
+bot.send_message("Hello there!", ["Receiver's Username"])
