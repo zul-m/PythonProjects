056f87ea88543122b1b93c780beceb644fc6752e
Zulfikar Muhamad <zul.m@icloud.com>
2022-11-07 17:56:05 +0800

    Added backward for loop directory

---
+++ beginner/backward for loop/backward_for_loop.py
@@ -0,0 +1,8 @@
+list_ = ["Noah", "Rio", "Bianca", "Shawn", "Iker"]
+
+for i in list_:
+    print(i)
+
+# A reversed() function that can be used to reverse the order of a Python object while using a for loop.
+for i in reversed(list_):
+    print(i)
