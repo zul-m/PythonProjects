18fe40da0e45c210bc7ff63cddfb3460e3dab873
Zulfikar Muhamad <zul.m@icloud.com>
2022-11-14 17:47:43 +0800

    Added mad libs game

---
+++ beginner/mad libs game/mad_libs.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+pluralNoun = input("Enter a plural noun: ")
+celebrity = input("Enter the name of a celebrity: ")
+
+print("Roses are", color)
+print(pluralNoun, "is blue")
+print("I like", celebrity)
