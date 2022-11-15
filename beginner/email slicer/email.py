f2993bb778f0fd38af55ce96db96b16c10489fa5
Zulfikar Muhamad <zul.m@icloud.com>
2022-11-15 12:09:16 +0800

    Added email slicer directory

---
+++ beginner/email slicer/email.py
@@ -0,0 +1,7 @@
+email = input("Enter your email: ").strip()
+
+username = email[:email.index("@")]
+domain_name = email[email.index("@") + 1:]
+
+format_ = (f"Your user name is '{username}' and your domain is '{domain_name}'")
+print(format_)
