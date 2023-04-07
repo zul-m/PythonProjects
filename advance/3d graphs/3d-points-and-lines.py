2fb664b905c69c61b7c7e46f6b489765c70c700b
Zulfikar Muhamad <zul.m@icloud.com>
2023-04-07 16:57:42 +0800

    Added 3d contour plots graph

--- advance/3d graphs/3d-points-and-lines.py
+++ advance/3d graphs/3d-points-and-lines.py
@@ -8,13 +8,13 @@ from mpl_toolkits.mplot3d import Axes3D
 z = np.linspace(0, 15, 1000)
 x = np.sin(z)
 y = np.cos(z)
-ax = plt.axes(projection='3d')
+ax = plt.axes(projection = '3d')
 ax.plot3D(x, y, z, 'grey')
 
 # Data for three dimensional scattered points.
 z = 15 * np.random.random(100)
 x = np.sin(z) + 0.1 * np.random.randn(100)
 y = np.cos(z) + 0.1 * np.random.randn(100)
-ax.scatter3D(x, y, z, c=z, cmap='Greens')
+ax.scatter3D(x, y, z, c = z, cmap = 'Greens')
 
 plt.show()
